ee1552cd8afde843b0b686a440dba4865779e1e9
Vresod <lukebinkofsky@gmail.com>
2021-04-03 18:47:49 -0400

    change capitalism question

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 questions =  [
-    {"id": 0, "question": "Do you like capitalism", "effects": {"right": 10}},
+    {"id": 0, "question": "Do you like social hierarchy", "effects": {"right": 10}},
     {"id": 1, "question": "Do you like goberment", "effects": {"auth": 10}},
     {"id": 2, "question": "Do you like technology", "effects": {"prog": 10}}
 ];
